1edbb7436593753bdd5499e29c5cb618fa05792f
BarneyLoosemore <biscuitfull@gmail.com>
2019-01-03 22:10:57 +0000

    ingreds + preparation

--- src/SelectedRecipe.js
+++ src/SelectedRecipe.js
@@ -6,12 +6,28 @@ class SelectedRecipe extends Component {
 
     render(){
         const { handleOnCardClick, recipe } = this.props
+        const ingredients = recipe.attributes["recipe-ingredients"]
+        const instructions = recipe.attributes.instructions
         return(
             <div className=''>
                 <RecipeCard recipe={recipe} handleOnCardClick={handleOnCardClick}/>
                 <hr/>
                 <div className="ingredients-tag">
                     Ingredients
+                </div>
+                <div className="ingredients-list">
+                    {
+                        ingredients.map(ing => <div className='ingredient-item'> {ing.content} </div>)
+                    }
+                </div>
+                <hr/>
+                <div className="ingredients-tag">
+                    Preparation
+                </div>
+                <div className="">
+                    {
+                        instructions.map(inst => <div className='ingredient-item'> {inst.index}. {inst.content} </div>)
+                    }
                 </div>
             </div>
             )
